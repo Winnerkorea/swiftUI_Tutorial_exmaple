5a0a42d0e00b249daf4f23ca8051ce5a6566d1c9
Winnerkorea <trueater@naver.com>
2025-09-08 22:09:05 +0900

    Presentation Detents

---
+++ SheetAndNavigation/SheetAndNavigation/PresentaionDetentView.swift
@@ -0,0 +1,18 @@
+//
+//  PresentaionDetentView.swift
+//  SheetAndNavigation
+//
+//  Created by Baba on 9/8/25.
+//
+
+import SwiftUI
+
+struct PresentaionDetentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PresentaionDetentView()
+}
