24a18187dc7eecb5fe1878f74a795e3907d8fa11
Winnerkorea <trueater@naver.com>
2025-09-08 19:30:02 +0900

    Setup Sheet And Navigation for swift

---
+++ SheetAndNavigation/SheetAndNavigation/SheetAndNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SheetAndNavigationApp.swift
+//  SheetAndNavigation
+//
+//  Created by Baba on 9/8/25.
+//
+
+import SwiftUI
+
+@main
+struct SheetAndNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
