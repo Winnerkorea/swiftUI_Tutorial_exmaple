25ace2a26069a570c2b89fe59232bfe5a559d229
Winnerkorea <68183508+Winnerkorea@users.noreply.github.com>
2025-09-13 07:13:30 +0900

    List View

--- ListScrollViews/ListScrollViews/ContentView.swift
+++ ListScrollViews/ListScrollViews/ContentView.swift
@@ -8,8 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var shoppingItems: [ShoppingModel] = [
+        ShoppingModel(title: "Apple", price: "$1.00"),
+        ShoppingModel(title: "Banana", price: "$0.50"),
+        ShoppingModel(title: "Orange", price: "$0.75")
+    ]
+    
+    @State private var toDoItems: [String] = [
+        "walk the dog",
+        "Wash clothes",
+        "Do homework"
+    ]
+    
+    
     var body: some View {
         VStack {
+            List(shoppingItems) { shoppingItem in
+                HStack{
+                    Text(shoppingItem.title)
+                    Spacer()
+                    Text(shoppingItem.price)
+                }
+            }
+            
+            List {
+                Text("An Item")
+                Image(systemName: "person.fill")
+                Button("Click Me") {
+                    // Action
+                    print("Click Ok")
+                }
+            }
+            List{
+                ForEach(shoppingItems) { shoppingItem in
+                    HStack{
+                        Text(shoppingItem.title)
+                        Spacer()
+                        Text(shoppingItem.price)
+                        
+                    }
+                }
+                Text("Another Item")
+                Text("Another Item 2")
+                ForEach(toDoItems, id:\.self) { toDoItem in
+                    Text(toDoItem)
+                }
+            }
          
         }
     }
---
+++ ListScrollViews/ListScrollViews/ShoppingModel.swift
@@ -0,0 +1,14 @@
+//
+//  ShoppingModel.swift
+//  ListScrollViews
+//
+//  Created by Winnerkorea on 9/13/25.
+//
+
+import Foundation
+
+struct ShoppingModel: Identifiable {
+    let id = UUID()
+    let title: String
+    let price: String
+}
