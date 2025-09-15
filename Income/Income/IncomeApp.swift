3811628c2115839ff8c5e9d36f1d503436c87c16
Winnerkorea <trueater@naver.com>
2025-09-15 17:00:42 +0900

    Income App

---
+++ Income/Income/IncomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeApp.swift
+//  Income
+//
+//  Created by Baba on 9/15/25.
+//
+
+import SwiftUI
+
+@main
+struct IncomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
