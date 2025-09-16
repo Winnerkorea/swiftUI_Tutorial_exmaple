95606cdfa1af5b3e5110a07e6e8a1674ab9bfd2e
Winnerkorea <trueater@naver.com>
2025-09-16 23:42:05 +0900

    To Do List

---
+++ ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Baba on 9/16/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
