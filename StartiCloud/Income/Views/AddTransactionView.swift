3793c07814e62a1572d52325acd1f7b58c865fa8
Winnerkorea <trueater@naver.com>
2025-09-26 11:36:50 +0900

    Start iCloud

---
+++ StartiCloud/Income/Views/AddTransactionView.swift
@@ -0,0 +1,109 @@
+//
+//  AddTransactionView.swift
+//  Income
+//
+//
+
+import SwiftUI
+import SwiftData
+
+struct AddTransactionView: View {
+    @State private var amount = 0.0
+    @State private var transactionTitle = ""
+    @State private var selectedTransactionType: TransactionType = .expense
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showAlert = false
+    var transactionToEdit: TransactionModel?
+    @Environment(\.dismiss) var dismiss
+    @Environment(\.modelContext) private var context
+    
+    @AppStorage("currency") var currency = Currency.usd
+    
+    var numberFormatter: NumberFormatter {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .currency
+        numberFormatter.locale = currency.locale
+        return numberFormatter
+    }
+    
+    var body: some View {
+        VStack {
+            TextField("0.00", value: $amount, formatter: numberFormatter)
+                .font(.system(size: 60, weight: .thin))
+                .multilineTextAlignment(.center)
+                .keyboardType(.numberPad)
+            Rectangle()
+                .fill(Color(uiColor: UIColor.lightGray))
+                .frame(height: 0.5)
+                .padding(.horizontal, 30)
+            Picker("Choose Type", selection: $selectedTransactionType) {
+                ForEach(TransactionType.allCases) { transactionType in
+                    Text(transactionType.title)
+                        .tag(transactionType)
+                }
+            }
+            TextField("Title", text: $transactionTitle)
+                .font(.system(size: 15))
+                .textFieldStyle(.roundedBorder)
+                .padding(.horizontal, 30)
+                .padding(.top)
+            Button(action: {
+                guard transactionTitle.count >= 2 else {
+                    alertTitle = "Invalid Title"
+                    alertMessage = "Title must be 2 or more characters long."
+                    showAlert = true
+                    return
+                }
+                
+                if let transactionToEdit = transactionToEdit {
+                    transactionToEdit.title = transactionTitle
+                    transactionToEdit.amount = amount
+                    transactionToEdit.type  = selectedTransactionType
+                } else {
+                    let transaction = TransactionModel(id: UUID(), title: transactionTitle, type: selectedTransactionType, amount: amount, date: Date())
+                    context.insert(transaction)
+                }
+                
+                dismiss()
+                
+            }, label: {
+                Text(transactionToEdit == nil ? "Create" : "Update")
+                    .font(.system(size: 15, weight: .semibold))
+                    .foregroundStyle(Color.white)
+                    .frame(height: 40)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.primaryLightGreen)
+                    .clipShape(RoundedRectangle(cornerRadius: 6))
+                    
+            })
+            .padding(.top)
+            .padding(.horizontal, 30)
+            Spacer()
+        }
+        .onAppear(perform: {
+            if let transactionToEdit = transactionToEdit {
+                amount = transactionToEdit.amount
+                transactionTitle = transactionToEdit.title
+                selectedTransactionType = transactionToEdit.type
+            }
+        })
+        .padding(.top)
+        .alert(alertTitle, isPresented: $showAlert) {
+            Button(action: {
+                
+            }, label: {
+                Text("OK")
+            })
+        } message: {
+            Text(alertMessage)
+        }
+
+    }
+}
+
+#Preview {
+    let previewContainer = PreviewHelper.previewContainer
+    return AddTransactionView()
+        .modelContainer(previewContainer)
+}
---
+++ StartiCloud/Income/Views/HomeView.swift
@@ -0,0 +1,180 @@
+//
+//  HomeView.swift
+//  Income
+//
+//
+
+
+import SwiftUI
+import SwiftData
+
+struct HomeView: View {
+    
+    @Query var transactions: [TransactionModel]
+    
+    @State private var showAddTransactionView = false
+    @State private var transactionToEdit: TransactionModel?
+    
+    @State private var showSettings = false
+    
+    @AppStorage("orderDescending") var orderDescending = false
+    @AppStorage("filterMinimum") var filterMinimum = 0.0
+    @AppStorage("currency") var currency = Currency.usd
+    
+    @Environment(\.modelContext) private var context
+    
+    private var numberFormatter: NumberFormatter {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .currency
+        numberFormatter.locale = currency.locale
+        return numberFormatter
+    }
+    
+    private var displayTransactions: [TransactionModel] {
+        let sortedTransactions = orderDescending ? transactions.sorted(by: { $0.date < $1.date }) : transactions.sorted(by: { $0.date > $1.date })
+        guard filterMinimum > 0 else {
+            return sortedTransactions
+        }
+        let filteredTransactions = sortedTransactions.filter({ $0.amount > filterMinimum })
+        return filteredTransactions
+    }
+    
+    private var expenses: String {
+        let sumExpenses = transactions.filter({ $0.type == .expense }).reduce(0, { $0 + $1.amount })
+        return numberFormatter.string(from: sumExpenses as NSNumber) ?? "$US0.00"
+    }
+    
+    private var income: String {
+        let sumIncome = transactions.filter({ $0.type == .income }).reduce(0, { $0 + $1.amount })
+        return numberFormatter.string(from: sumIncome as NSNumber) ?? "$US0.00"
+    }
+    
+    private var total: String {
+        let sumExpenses = transactions.filter({ $0.type == .expense }).reduce(0, { $0 + $1.amount })
+        let sumIncome = transactions.filter({ $0.type == .income }).reduce(0, { $0 + $1.amount })
+        let total = sumIncome - sumExpenses
+        return numberFormatter.string(from: total as NSNumber) ?? "$US0.00"
+    }
+    
+    fileprivate func FloatingButton() -> some View {
+        VStack {
+            Spacer()
+            NavigationLink {
+                AddTransactionView()
+            } label: {
+                Text("+")
+                    .font(.largeTitle)
+                    .frame(width: 70, height: 70)
+                    .foregroundStyle(Color.white)
+                    .padding(.bottom, 7)
+                    
+            }
+            .background(Color.primaryLightGreen)
+            .clipShape(Circle())
+        }
+    }
+    
+    fileprivate func BalanceView() -> some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 8)
+                .fill(Color.primaryLightGreen)
+            VStack(alignment: .leading, spacing: 8) {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("BALANCE")
+                            .font(.caption)
+                            .foregroundStyle(Color.white)
+                        Text("\(total)")
+                            .font(.system(size: 42, weight: .light))
+                            .foregroundStyle(Color.white)
+                    }
+                    Spacer()
+                }
+                .padding(.top)
+                
+                HStack(spacing: 25) {
+                    VStack(alignment: .leading) {
+                        Text("Expense")
+                            .font(.system(size: 15, weight: .semibold))
+                            .foregroundStyle(Color.white)
+                        Text("\(expenses)")
+                            .font(.system(size: 15, weight: .regular))
+                            .foregroundStyle(Color.white)
+                    }
+                    VStack(alignment: .leading) {
+                        Text("Income")
+                            .font(.system(size: 15, weight: .semibold))
+                            .foregroundStyle(Color.white)
+                        Text("\(income)")
+                            .font(.system(size: 15, weight: .regular))
+                            .foregroundStyle(Color.white)
+                    }
+                }
+                Spacer()
+            }
+            .padding(.horizontal)
+        }
+        .shadow(color: Color.black.opacity(0.3), radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x: 0, y: 5)
+        .frame(height: 150)
+        .padding(.horizontal)
+    }
+    
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                VStack {
+                    BalanceView()
+                    List {
+                        ForEach(displayTransactions) { transaction in
+                            Button(action: {
+                                transactionToEdit = transaction
+                            }, label: {
+                                TransactionView(transaction: transaction)
+                                    .foregroundStyle(.black)
+                            })
+                        }
+                        .onDelete(perform: delete)
+                    }
+                    .scrollContentBackground(.hidden)
+                }
+                FloatingButton()
+            }
+            .sheet(isPresented: $showSettings, content: {
+                SettingsView()
+            })
+            .navigationTitle("Income")
+            .navigationDestination(item: $transactionToEdit, destination: { transactionToEdit in
+                AddTransactionView(transactionToEdit: transactionToEdit)
+            })
+            .navigationDestination(isPresented: $showAddTransactionView, destination: {
+                AddTransactionView()
+            })
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button(action: {
+                        showSettings = true
+                    }, label: {
+                        Image(systemName: "gearshape.fill")
+                            .foregroundStyle(Color.black)
+                    })
+                }
+            }
+        }
+    }
+    
+    private func delete(at offsets: IndexSet) {
+        for index in offsets {
+            context.delete(transactions[index])
+            try? context.save()
+        }
+    }
+    
+}
+
+#Preview {
+    let previewContainer = PreviewHelper.previewContainer
+    return HomeView()
+        .modelContainer(previewContainer)
+}
+
+
